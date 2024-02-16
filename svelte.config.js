af940ef72f0c452cb697c0b3de678ca5597edd3c
Sung Jun, Hwang <sungjun.hw@gmail.com>
2024-02-16 13:33:47 +0900

    change set

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
+import azure from 'svelte-adapter-azure-swa';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,11 +14,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			out: 'public'
-			}
-		)
+		// adapter: adapter({
+		// 	out: 'public'
+		// 	}
+		// )
+		adapter: azure()
 	},
 };
 
 export default config;
+ß
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	build: {
+		outDir: 'public' // 빌드 결과물의 위치
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
